624dfed318e8445a7b243dc0aa0f2ba5cbaa60fc
MaxKuzya13 <maxit2301@gmail.com>
2023-10-07 13:36:56 +0300

    Close #19: install theme

---
+++ web/themes/custom/kenny/js/kenny.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * kenny behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.kenny = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
